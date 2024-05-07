b46533f0f1369e024ad0c10e6fa395b971635321
Julia Amorim <juliaacarvalhosa@gmail.com>
2024-05-06 23:00:37 -0300

    'v2'

--- ServidorVolume.sql
+++ ServidorVolume.sql
@@ -8,13 +8,9 @@ CONSTRAINT [PK_ServidorVolume] PRIMARY KEY CLUSTERED
 	[Hostname] ASC,
 	[Volume] ASC,
 	[Coleta] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+))
 GO
  
 ALTER TABLE [dbo].[ServidorVolume]  WITH CHECK ADD  CONSTRAINT [FK_ServidorVolume_Volume] FOREIGN KEY([Hostname], [Volume])
 REFERENCES [dbo].[Volume] ([Hostname], [Volume])
-GO
- 
-ALTER TABLE [dbo].[ServidorVolume] CHECK CONSTRAINT [FK_ServidorVolume_Volume]
 GO
