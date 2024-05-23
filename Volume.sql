b9733ee3af6f59744cae09b3e59312022a1e9bb7
Júlia Carvalhosa <juliaacarvalhosa@gmail.com>
2024-05-23 10:33:52 -0300

    Update Volume.sql

--- Volume.sql
+++ Volume.sql
@@ -3,8 +3,8 @@ CREATE TABLE [dbo].[Volume](
 	[Volume] [char](1) NOT NULL,
 	[CapacidadeGB] [numeric](10, 2) NOT NULL,
 	[Coleta] [smalldatetime] NULL,
-    [ExcluirMonitoramento] BIT DEFAULT 0, -- Flag caso exista a necessidade de exlusão de algum servidor em alguma consulta
-    [LimiteAlerta] INT DEFAULT 5 -- Threshold com o limite 5
+    	[ExcluirMonitoramento] BIT DEFAULT 0, -- Flag caso exista a necessidade de exlusão de algum servidor em alguma consulta
+    	[LimiteAlerta] INT DEFAULT 5 -- Threshold com o limite 5
 CONSTRAINT [PK_Volume] PRIMARY KEY CLUSTERED 
 (
 	[Hostname] ASC,
