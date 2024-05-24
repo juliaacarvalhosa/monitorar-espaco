566d377caf07f06ae0455e77c801a620df9dcdbb
Júlia Carvalhosa <20212105186@veigadealmeida.edu.br>
2024-05-24 11:52:26 -0300

    Adicionando percentual de espaço ocupado na view

--- MonitorEspaçoLivre.sql
+++ MonitorEspaçoLivre.sql
@@ -1,14 +1,17 @@
--- Criação da View usada na consulta da procedure "EspacoDisco"
-
-CREATE VIEW [dbo].[MonitorEspacoLivre]
+CREATE OR ALTER VIEW [dbo].[MonitorEspacoLivre]
 AS
 SELECT     v.Hostname, v.Volume, 
-		   v.CapacidadeGB, 
-           CAST(SV.EspacoLivre / 1024.0 AS numeric(10, 2)) AS EspacoLivreGB, 
-           v.CapacidadeGB - CAST(SV.EspacoLivre / 1024.0 AS numeric(10, 2)) as [Espaço Ocupado], -- calcula o espaço ocupado 
-           CAST(SV.EspacoLivre / 1024.0 / v.CapacidadeGB * 100 AS numeric(10, 2)) AS PercentualLivre, -- porcentagem de espaço livre
-		   v.ExcluirMonitoramento, v.LimiteAlerta
-FROM         dbo.Volume AS v INNER JOIN
-                      dbo.ServidorVolume AS SV ON v.Volume = SV.Volume AND v.Hostname = HSV.Hostname
-WHERE      floor(cast (sv.Coleta as real)) + 1 > floor(cast(getdate() as real)) 
+           v.CapacidadeGB, 
+           CAST(HSV.EspacoLivre / 1024.0 AS numeric(10, 2)) AS EspacoLivreGB, 
+           v.CapacidadeGB - CAST(HSV.EspacoLivre / 1024.0 AS numeric(10, 2)) as EspacoOcupado,
+           CAST((v.CapacidadeGB - CAST(HSV.EspacoLivre / 1024.0 AS numeric(10, 2))) / v.CapacidadeGB * 100 AS numeric(10, 2)) AS PercentualOcupado,
+           CAST(HSV.EspacoLivre / 1024.0 / v.CapacidadeGB * 100 AS numeric(10, 2)) AS PercentualLivre,
+           v.ExcluirMonitoramento, v.LimiteAlerta
+FROM       dbo.Volume AS v 
+INNER JOIN (
+    SELECT Hostname, Volume, EspacoLivre, Coleta,
+           ROW_NUMBER() OVER (PARTITION BY Hostname, Volume ORDER BY Coleta DESC) AS rn
+    FROM dbo.Historico_Servidor_Volume
+) AS HSV ON v.Volume = HSV.Volume AND v.Hostname = HSV.Hostname AND HSV.rn = 1
+WHERE      floor(cast (Hsv.Coleta as real)) + 1 > floor(cast(getdate() as real))
 GO
